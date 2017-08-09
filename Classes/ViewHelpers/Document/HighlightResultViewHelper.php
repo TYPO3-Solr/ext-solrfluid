f0c7c30b9e6cd0300e617b1c2f7cb4b37d22ad22
Rafael Kähm <rafael.kaehm@dkd.de>
2017-08-09 18:16:09 +0200

    [TASK] Backport: GH #1487 Escape everything in HighlightingResultViewHelper except configured wrap
    This pr:
        Modifies the HighlightingResultViewHelper to escape everything except the configured highlighting wrap.
    
    comes from: https://github.com/TYPO3-Solr/ext-solr/pull/1487

--- Classes/ViewHelpers/Document/HighlightResultViewHelper.php
+++ Classes/ViewHelpers/Document/HighlightResultViewHelper.php
@@ -14,7 +14,6 @@ namespace ApacheSolrForTypo3\Solrfluid\ViewHelpers\Document;
  * The TYPO3 project - inspiring people to share!
  */
 
-use ApacheSolrForTypo3\Solr\Util;
 use ApacheSolrForTypo3\Solrfluid\Domain\Search\ResultSet\SearchResultSet;
 use ApacheSolrForTypo3\Solrfluid\ViewHelpers\AbstractViewHelper;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -36,29 +35,30 @@ class HighlightResultViewHelper extends AbstractViewHelper implements Compilable
     protected $escapeOutput = false;
 
     /**
-     * @param SearchResultSet $resultSet
-     * @param \Apache_Solr_Document $document
-     * @param string $fieldName
-     * @return string
+     * Initializes the arguments
      */
-    public function render(SearchResultSet $resultSet, \Apache_Solr_Document $document, $fieldName)
+    public function initializeArguments()
     {
-        return self::renderStatic(
-            [
-                'resultSet' => $resultSet,
-                'document' => $document,
-                'fieldName' => $fieldName
-            ],
+        parent::initializeArguments();
+        $this->registerArgument('resultSet', SearchResultSet::class, 'The context searchResultSet', true);
+        $this->registerArgument('document', \Apache_Solr_Document::class, 'The document to highlight', true);
+        $this->registerArgument('fieldName', 'string', 'The fieldName', true);
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function render()
+    {
+        return static::renderStatic(
+            $this->arguments,
             $this->buildRenderChildrenClosure(),
             $this->renderingContext
         );
     }
 
     /**
-     * @param array $arguments
-     * @param callable $renderChildrenClosure
-     * @param RenderingContextInterface $renderingContext
-     * @return string
+     * @inheritdoc
      */
     public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
     {
@@ -66,13 +66,48 @@ class HighlightResultViewHelper extends AbstractViewHelper implements Compilable
         $resultSet = $arguments['resultSet'];
         $fieldName = $arguments['fieldName'];
         $document = $arguments['document'];
+        $content = self::getHighlightedContent($resultSet, $document, $fieldName);
+        return self::escapeEverythingExceptAllowedTags($resultSet, $content);
+    }
 
+    /**
+     * @param SearchResultSet $resultSet
+     * @param $document
+     * @param $fieldName
+     * @return mixed|string
+     */
+    protected static function getHighlightedContent(SearchResultSet $resultSet, $document, $fieldName)
+    {
         $fragmentSeparator = $resultSet->getUsedSearchRequest()->getContextTypoScriptConfiguration()->getSearchResultsHighlightingFragmentSeparator();
         $content = call_user_func([$document, 'get' . $fieldName]);
         $highlightedContent = $resultSet->getUsedSearch()->getHighlightedContent();
         if (!empty($highlightedContent->{$document->getId()}->{$fieldName}[0])) {
             $content = implode(' ' . $fragmentSeparator . ' ', $highlightedContent->{$document->getId()}->{$fieldName});
+            return $content;
         }
         return $content;
+    }
+
+    /**
+     * @param SearchResultSet $resultSet
+     * @param $content
+     * @return string
+     */
+    protected static function escapeEverythingExceptAllowedTags(SearchResultSet $resultSet, $content)
+    {
+        $wrap = $resultSet->getUsedSearchRequest()->getContextTypoScriptConfiguration()->getSearchResultsHighlightingWrap();
+        if ($wrap === '') {
+            return htmlspecialchars($content);
+        }
+        $wrapParts = GeneralUtility::trimExplode("|", $wrap);
+        if (count($wrapParts) !== 2) {
+            return htmlspecialchars($content);
+        }
+        $substitutedContent = str_replace($wrapParts[0], '___highlight_begin___', $content);
+        $substitutedContent = str_replace($wrapParts[1], '___highlight_end___', $substitutedContent);
+        $output = htmlspecialchars($substitutedContent);
+        $output = str_replace('___highlight_begin___', $wrapParts[0], $output);
+        $output = str_replace('___highlight_end___', $wrapParts[1], $output);
+        return $output;
     }
 }
